5402ea0114d731c6cb4bc408cd727a714082ab83
MahmoudHassann <mahmoud.m.azab@gmail.com>
2024-10-17 23:43:47 +0300

    app router

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import session from 'express-session'
-import MongoStore from 'connect-mongo'
 import cookieParser from 'cookie-parser'
 import {appRouter} from './modules/index.router.js'
 dotenv.config({ path: './config/.env' });
@@ -22,26 +20,6 @@ appRouter(app)
 
 
 
-// Session configuration
-app.use(session({
-  name: process.env.SESSION_COOKIE_NAME,
-  secret: process.env.SUPER_SECRET_KEY,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URL,
-    ttl: 60 * 60 * 24, // 1 day in seconds,
-    touchAfter: 24 * 3600, // time period in seconds
-    autoRemove: 'native' // Enable automatic removal of expired sessions
-  }),
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24, // 1 day in milliseconds
-    sameSite: 'strict'
-  },
-  rolling: true,
-}));
 
 
 
--- modules/index.router.js
+++ modules/index.router.js
@@ -9,14 +9,37 @@ import couponRouter from "./coupon/coupon.router.js"
 import { globalErrorHandling } from '../services/errorhandling.js';
 import connectDB from '../db/mongoose.js'
 import express from "express"
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
 
 
 
 export const appRouter = (app) => {
 
     app.use(express.json())
-
     const BaseURL = process.env.BASE_URL
+
+
+    // Session configuration
+    app.use(session({
+        name: process.env.SESSION_COOKIE_NAME,
+        secret: process.env.SUPER_SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URL,
+            ttl: 60 * 60 * 24, // 1 day in seconds,
+            touchAfter: 24 * 3600, // time period in seconds
+            autoRemove: 'native' // Enable automatic removal of expired sessions
+        }),
+        cookie: {
+            secure: process.env.NODE_ENV === 'production',
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24, // 1 day in milliseconds
+            sameSite: 'strict'
+        },
+        rolling: true,
+    }));
 
     app.use(`${BaseURL}/auth`, authRouter)
     app.use(`${BaseURL}/subCategory`, subCategoryRouter)
